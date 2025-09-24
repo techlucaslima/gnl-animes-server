377af96e8d74492003a9e0c19b2168f34c70cbbe
Lucas Lima <mosukasangamer@gmail.com>
2025-09-24 14:57:50 -0300

    cors dev add

--- src/app.js
+++ src/app.js
@@ -1,14 +1,22 @@
 import express from "express";
 import animesRoutes from "./routes/animesRoutes.js";
 import dotenv from "dotenv";
+import cors from "cors"; 
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: false
+}));
 
 app.use("/animes", animesRoutes);
 
-app.listen(process.env.PORT, () =>{
+app.listen(PORT, () =>{
   console.log("Server running on http://localhost:3000");
 });
+

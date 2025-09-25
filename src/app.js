7484a088680c1314c561fe7becabbffbfc4d9adf
Lucas Lima <mosukasangamer@gmail.com>
2025-09-25 14:13:07 -0300

    Add production url

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173', 'https://gnl-animes.up.railway.app'],
   credentials: false
 }));
 
